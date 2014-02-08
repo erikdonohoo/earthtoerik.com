f37b53f2dcb289069c68f116771344099fa216fc
Erik Donohoo <erikdonohoo@gmail.com>
2014-02-08 15:24:21 -0700

    more work with color scheme, header and fonts

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,24 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngRoute','ngResource']).
+angular.module('ed.web', ['ngRoute','ngResource']).
   config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode(true);
 }])
 
-.run(['$rootScope', function($root){
-	$root.dude = 'hey';
-}])
+// Logo Directive
+.directive('edLogo', [function(){
+	var dir = {};
+	dir.restrict = 'E';
+	dir.template = '<div ng-style="style" class="logo">{<span>e</span>}</div>';
+	dir.scope = {
+		size:'@'
+	};
+	dir.link = function(scope) {
+		scope.style = {};
+		scope.style['font-size'] = scope.size + 'px';
+		console.log(scope.style);
+	};
+
+	return dir;
+}]);
