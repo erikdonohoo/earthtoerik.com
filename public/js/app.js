41e138594163459056418871c0db07e0d963ae0d
Erik Donohoo <erikdonohoo@gmail.com>
2014-02-08 11:13:29 -0700

    Added color palette, working on header

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,11 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+angular.module('myApp', ['ngRoute','ngResource']).
+  config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode(true);
-  }]);
+}])
+
+.run(['$rootScope', function($root){
+	$root.dude = 'hey';
+}])
