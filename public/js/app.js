2c641f2ca7399780a38cbf916d8a8a63bbb226ec
Erik Donohoo <erikdonohoo@gmail.com>
2014-02-12 20:57:11 -0700

    Better frontend searching for posts

--- public/js/app.js
+++ public/js/app.js
@@ -191,6 +191,30 @@ angular.module('ed.web', ['ngRoute','ngResource','ngTouch','ngAnimate']).
 	return dir;
 }])
 
+.filter('objectFilter', ['$filter', function($filter) {
+	
+	return function(list, query) {
+		if(!query)
+			return list;
+		if(query['$'] != null)
+			query = query['$'];
+
+		var queries = query.split(' ');
+
+		var newlist = list.slice(0);
+		for (var i = queries.length - 1; i >= 0; i--) {
+			var results = $filter('filter')(list,queries[i]);
+			for (var j = newlist.length - 1; j >= 0; j--) {
+				if(!~results.indexOf(newlist[j])) {
+					newlist.splice(j,1);
+				}
+			}
+		}
+
+		return newlist;
+	};
+}])
+
 .factory('Nav', [function(){
 	var nav = {};
 	nav.open = false; // Show/hide nav
